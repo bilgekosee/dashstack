a54a9755ae45529bf9638cadc86596802346c2a0
bilgekosee <bilge.koosee@gmail.com>
2025-02-05 20:30:47 +0300

    header add

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,8 @@
 import "./Header.css";
 import { FiBox } from "react-icons/fi";
 import { FaSearch } from "react-icons/fa";
+import { CiSettings } from "react-icons/ci";
+import { MdOutlineNotificationAdd } from "react-icons/md";
 
 const Header = () => {
   return (
@@ -24,13 +26,35 @@ const Header = () => {
             <input
               className="search-input"
               type="text"
-              placeHolder="Type any cryptocurrency..."
-            ></input>
+              placeholder="Type any cryptocurrency..."
+            />
           </div>
         </div>
       </div>
-      <div className="admin"></div>
+      <div className="dash-stack-right">
+        <img
+          className="profile-admin-photo"
+          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="profile"
+        />
+
+        <div className="icon-container">
+          <CiSettings className="setting-icon" />
+        </div>
+
+        <div className="icon-container">
+          <MdOutlineNotificationAdd className="setting-icon" />
+          <span className="badge">3</span>
+        </div>
+
+        <img
+          className="flag"
+          src="https://images.unsplash.com/photo-1562884328-39da45501a9c?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="flag"
+        />
+      </div>
     </div>
   );
 };
+
 export default Header;
